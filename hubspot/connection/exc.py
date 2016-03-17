ba25afe739b0a0da2480e3b760faa3b3830de56b
Gil Gonçalves <lursty@gmail.com>
2016-03-17 11:29:05 +0000

    Revert "Implemented str method for HubspotServerError"
    This reverts commit 18a87d3ef72b9537db016bb12368bf7b8ae5d776.

--- hubspot/connection/exc.py
+++ hubspot/connection/exc.py
@@ -60,9 +60,6 @@ class HubspotServerError(HubspotException):
 
         self.http_status_code = http_status_code
 
-    def __str__(self):
-        return repr(self)
-
     def __repr__(self):
         return '{} {}'.format(self.http_status_code, self.message)
 
